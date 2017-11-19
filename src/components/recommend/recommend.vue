236421b26b39906edc2e6c3ff60fbfb93130f779
frwupeng517 <710186001@qq.com>
2017-11-19 22:21:00 +0800

    page structer

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="rank">
+        推荐
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="rank">
+        歌手
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
