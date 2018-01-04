25e6e05261834a10cc88b2b477c37e7215cedec0
frwupeng517 <710186001@qq.com>
2018-01-04 23:47:04 +0800

    finish song list

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,49 +1,55 @@
 <template>
     <div class="recommend" ref="recommend">
-        <div class="recommend-content">
-            <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-                <slider>
-                    <div v-for="(item, index) in recommends">
-                        <a :href="item.linkUrl">
-                            <img :src="item.picUrl" alt="">
-                        </a>
-                    </div>
-                </slider>
+        <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+                    <slider>
+                        <div v-for="(item, index) in recommends">
+                            <a :href="item.linkUrl" @load="loadImage">
+                                <!-- needsclick 这个class是用于使轮播图可点击，因为引入scroll的click为true，与fastclick冲突导致点击失效 -->
+                                <img class="needsclick" :src="item.picUrl" alt="">
+                            </a>
+                        </div>
+                    </slider>
+                </div>
+                <div class="recommend-list">
+                    <h2 class="list-title">电台</h2>
+                    <ul class="list-container">
+                        <li v-for="radio in radioList" class="item">
+                            <a href="https://i.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=104251904,201844913,200792833,204179897,212448952,105561979,105391499,203801126,208482132,202915119,202979233,211997756,204289330,105621350,102950518,205618103,202665327,211941841,204270540,105511154">
+                                <div class="list-media">
+                                    <img :src="radio.picUrl" alt="">
+                                    <span class="icon icon-play"></span>
+                                </div>
+                                <div class="list-info">
+                                    <h3>{{radio.Ftitle}}</h3>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单</h1>
+                    <ul class="list-container">
+                        <li v-for="disc in discList" class="item">
+                            <a :href="songListUrl+disc.id">
+                                <div class="list-media">
+                                    <img v-lazy="disc.picUrl" alt="">
+                                    <span class="icon icon-play"></span>
+                                </div>
+                                <div class="list-info">
+                                    <h3>{{disc.songListDesc}}</h3>
+                                    <p>{{disc.songListAuthor}}</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="recommend-list">
-                <h2 class="list-title">电台</h2>
-                <ul class="list-container">
-                    <li v-for="radio in radioList" class="item">
-                        <a href="https://i.y.qq.com/v8/playsong.html?ADTAG=myqq&from=myqq&channel=10007100&songid=104251904,201844913,200792833,204179897,212448952,105561979,105391499,203801126,208482132,202915119,202979233,211997756,204289330,105621350,102950518,205618103,202665327,211941841,204270540,105511154">
-                            <div class="list-media">
-                                <img :src="radio.picUrl" alt="">
-                                <span class="icon icon-play"></span>
-                            </div>
-                            <div class="list-info">
-                                <h3>{{radio.Ftitle}}</h3>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
+            <div class="loading-container" v-show="!discList.length">
+                <loading></loading>
             </div>
-            <div class="recommend-list">
-                <h1 class="list-title">热门歌单</h1>
-                <ul class="list-container">
-                    <li v-for="disc in discList" class="item">
-                        <a :href="songListUrl+disc.id">
-                            <div class="list-media">
-                                <img :src="disc.picUrl" alt="">
-                                <span class="icon icon-play"></span>
-                            </div>
-                            <div class="list-info">
-                                <h3>{{disc.songListDesc}}</h3>
-                                <p>{{disc.songListAuthor}}</p>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </scroll>
     </div>
 </template>
 
@@ -52,6 +58,8 @@ import BScroll from 'better-scroll'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 export default {
     data () {
         return {
@@ -62,7 +70,7 @@ export default {
         }
     },
     components: {
-        Slider
+        Slider, Scroll, Loading
     },
     created () {
         this._getRecommend()
@@ -77,6 +85,14 @@ export default {
                     this.discList = res.data.songList
                 }
             })
+        },
+        // 轮播图中图片加载成功以后调用重新刷新
+        loadImage () {
+            // 默认是没有checkLoaded属性的，通过这种写法只执行一次，避免重复调用该方法
+            if (!this.checkLoaded) {
+                this.$refs.scroll.refresh()
+                this.checkLoaded = true
+            }
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 require('common/scss/index.scss')
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
